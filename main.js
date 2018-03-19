4e5de55e1a1f4d8387e718edfe3d14b51896779b
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-19 12:48:57 +0000

    Added a toggle function in CodeYourFuture button

---
+++ main.js
@@ -0,0 +1,36 @@
+{
+  /* <input type="button" id="button" value="Button">
+<script>
+  button.onclick = function() {
+    alert('Click!');
+  };
+</script>
+ */
+}
+
+function myFunction() {
+  var x = document.getElementById("CodeYourFuture");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// function myFunction() {
+//   var x = document.getElementById("udacity");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+// function main() {
+//   $("skillset").hide();
+//   $("projects-button").on("click", function() {
+//     $(".projects").toggle();
+//   });
+// }
+
+// $(document).ready(main);
