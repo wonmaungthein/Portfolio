1347f600ab59585cad5c6717a667436534bc1396
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-19 22:01:57 +0000

    done some changes in my draft

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-{
-  /* <input type="button" id="button" value="Button">
-<script>
-  button.onclick = function() {
-    alert('Click!');
-  };
-</script>
- */
-}
-
-// function myFunction() {
-//   var CodeYourFuture = document.querySelector("#CodeYourFuture");
-//   if (CodeYourFuture.style.display === "none") {
-//     CodeYourFuture.style.display = "block";
-//   } else {
-//     CodeYourFuture.style.display = "none";
-//   }
-// }
-
 var CodeYourFuture = document.querySelector(".codeYourFuture");
-
 function myFunction() {
   var CodeYourFuture = document.querySelector("#CodeYourFuture");
   if (CodeYourFuture.style.display === "none") {
@@ -27,12 +7,3 @@ function myFunction() {
     CodeYourFuture.style.display = "none";
   }
 }
-
-// function myFunction() {
-//   var x = document.getElementById("udacity");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
